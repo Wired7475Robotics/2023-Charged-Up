1fb96015f0bef5a3949c4947c39c30155317c1aa
GeraldTM <99144289+GeraldTM@users.noreply.github.com>
2023-02-10 11:24:37 -0500

    add linear acceleration

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -42,10 +42,10 @@ public class DriveTrain extends SubsystemBase{
     private Timer timer;
     public DriveTrain() {
         timer = new Timer();
-        leftDrive1 = new CANSparkMax(1, MotorType.kBrushless);
-        leftDrive2 = new CANSparkMax(9, MotorType.kBrushless);
-        rightDrive1 = new CANSparkMax(10, MotorType.kBrushless);
-        rightDrive2 = new CANSparkMax(19, MotorType.kBrushless);
+        leftDrive1 = new CANSparkMax(1, MotorType.kBrushless).setOpenLoopRampRate(0.5);
+        leftDrive2 = new CANSparkMax(9, MotorType.kBrushless)setOpenLoopRampRate(0.5);
+        rightDrive1 = new CANSparkMax(10, MotorType.kBrushless)setOpenLoopRampRate(0.5);
+        rightDrive2 = new CANSparkMax(19, MotorType.kBrushless)setOpenLoopRampRate(0.5);
         m_leftDrive = new MotorControllerGroup(leftDrive1, leftDrive2);
         m_rightDrive = new MotorControllerGroup(rightDrive1, rightDrive2);
         m_leftDrive.setInverted(true);
